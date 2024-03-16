63063390d3fce7bb25a919abb711339ebe1773ec
Goutham Krishna Shabu <gouthamkrish@proton.me>
2024-03-15 19:58:53 -0600

    hyprland config

---
+++ hypr/scripts/battery-mode.sh
@@ -0,0 +1,6 @@
+#Need to work on this
+# Copy the hyprdots gamemode sh file
+# Change my asusctl settings
+# 	Should lower kde board brightness
+# 	Should change asusctl profile to quiet
+# 	Should change asusctl fan to quiet
