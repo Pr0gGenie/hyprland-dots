868516bc7221a9eb5851f1bc42715d3f0daadb08
Goutham Krishna Shabu <gouthamkrish@proton.me>
2024-03-16 19:39:54 -0600

    Remove the accident directory

--- hypr/scripts/battery-mode.sh
+++
@@ -1,6 +0,0 @@
-#Need to work on this
-# Copy the hyprdots gamemode sh file
-# Change my asusctl settings
-# 	Should lower kde board brightness
-# 	Should change asusctl profile to quiet
-# 	Should change asusctl fan to quiet
